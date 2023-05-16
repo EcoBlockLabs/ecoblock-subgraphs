abb398298d9fa559c1f9d6c6ca798fa3268d6aff
Brian <briandmauro@gmail.com>
2023-05-16 08:06:32 +0700

    Fix l2StandardGateway condition empty

--- packages/layer2-token-gateway/metadata.template.ts
+++ packages/layer2-token-gateway/metadata.template.ts
@@ -1,6 +1,14 @@
-import { Address } from "@graphprotocol/graph-ts";
+import {Address, log} from "@graphprotocol/graph-ts";
 
 // export const nitroStartBlock = BigInt.fromString("{{{ nitroGenesisBlockNum }}}")
 
 // we use moustache to template these values in (as used in the subgraph manifest template)
-export const L2_STD_GATEWAY = Address.fromString("{{{ l2StandardGateway }}}".toLowerCase())
+const l2StandardGateway: string = "{{{ l2StandardGateway }}}"
+let l2StandardGatewayAddr = Address.zero()
+if (l2StandardGateway != "") {
+  log.info("l2StandardGateway not empty: %s", [l2StandardGateway])
+  l2StandardGatewayAddr = Address.fromString(l2StandardGateway.toLowerCase())
+} else {
+  log.info("l2StandardGateway empty: %s", [l2StandardGateway])
+}
+export const L2_STD_GATEWAY = l2StandardGatewayAddr
